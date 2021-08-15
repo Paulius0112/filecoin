5fdc900fb493bbc8a3d7e5796dc197adaff66e22
Paulius <paulius@Pauliuss-MacBook-Pro.local>
2021-08-15 17:41:05 +0300

    updated readme

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,12 @@ provider "linode" {
   token = var.linode_token
 }
 
+provider "google" {
+  credentials = file(var.gcp_credentials_file)
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+  zone        = var.gcp_zone
+}
 
 resource "random_password" "linode_root_password" {
   length           = 16
@@ -28,12 +34,34 @@ resource "linode_instance" "filecoin_node" {
   root_pass       = random_password.linode_root_password.result
 }
 
-resource "linode_instance" "monitoring" {
-  image           = "linode/ubuntu18.04"
-  label           = "monitoring"
-  group           = "terraform"
-  region          = "eu-central"
-  type            = "g6-standard-4"
-  authorized_keys = [chomp(file(var.ssh_public_key_file))]
-  root_pass       = random_password.linode_root_password.result
+resource "google_compute_instance" "monitoring_node" {
+  name         = "monitoring_node"
+  machine_type = "e2-standard-4"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = "default"
+    access_config {
+    }
+  }
+
+  metadata = {
+    ssh-keys = "filecoin:${file(var.ssh_public_key_file)}"
+  }
+}
+
+resource "google_compute_firewall" "default" {
+ name    = "wireguard"
+ network = "default"
+
+ allow {
+   protocol = "udp"
+   ports    = ["0-65535"]
+ }
 }
